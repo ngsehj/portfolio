a2cbd2bc9a939433a18d5200181e4985f4c60d15
Hyejin <ngsehj@gmail.com>
2023-07-29 15:45:33 +0900

    setting

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from 'pages/home/Home';
+
+export const GlobalDataContext = React.createContext();
+
+function App() {
+  const initGlobalData = {
+    viewportHeight: window.innerHeight,
+    viewportWidth: window.innerWidth,
+  };
+
+  const [globalData, setGlobalData] = useState(initGlobalData);
+
+  const handleResize = useCallback(() => {
+    const viewportHeight = window.innerHeight;
+    const viewportWidth = window.innerWidth;
+
+    setGlobalData(() => ({
+      ...globalData,
+      viewportHeight,
+      viewportWidth,
+    }));
+  }, [globalData]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [handleResize]);
+
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="App">
+          <GlobalDataContext.Provider value={globalData}>
+            {/* <Header /> */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+            </Routes>
+            {/* <Footer />
+            <TopButton /> */}
+          </GlobalDataContext.Provider>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
