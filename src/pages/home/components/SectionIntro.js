128f1f03c9c15b72415fd5161d937b16b8c9c59d
ngsehj <ngsehj@gmail.com>
2023-08-12 09:25:02 +0900

    intro section bg 수정

--- src/pages/home/components/SectionIntro.js
+++ src/pages/home/components/SectionIntro.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { useScrollCalc, useScrollTranslate } from 'hooks';
+import { useScrollCalc, useScrollFadeIn, useScrollTranslate } from 'hooks';
 import { ScrollPageContext } from 'pages/home/Home';
 import RotateCircleButton from 'components/RotateCircleButton';
 
@@ -7,8 +7,9 @@ const SectionIntro = () => {
   const text = ['꾸준히 성장하는', '웹 퍼블리셔 이야기', 'Web Publisher', 'Portfolio'];
   const animatedElement = {
     heading: useScrollCalc('opacity', { start: 3, end: -1 }),
-    0: useScrollTranslate(1, 0.2),
-    1: useScrollTranslate(1, 0.4),
+    bg: useScrollFadeIn('no', 1, 0.3),
+    0: useScrollTranslate(1, 0.4),
+    1: useScrollTranslate(1, 0.5),
     2: useScrollTranslate(1, 0.6),
     3: useScrollTranslate(1, 0.7),
   };
@@ -23,16 +24,18 @@ const SectionIntro = () => {
 
   return (
     <section className="section section-intro">
-      <RotateCircleButton text="Go portfolio * Go Portfolio * Go Portfolio * * * * * * " handleClick={handleClick} />
-      <h1 className="intro__heading" {...animatedElement.heading}>
-        {text.map((item, idx) => (
-          <div className="inner" key={idx}>
-            <span className={`heading${idx}`} {...animatedElement[idx]} key={item}>
-              {item}
-            </span>
-          </div>
-        ))}
-      </h1>
+      <div className="section__inner" {...animatedElement.bg}>
+        <RotateCircleButton text="Go portfolio * Go Portfolio * Go Portfolio * * * * * * " handleClick={handleClick} />
+        <h1 className="intro__heading" {...animatedElement.heading}>
+          {text.map((item, idx) => (
+            <div className="inner" key={idx}>
+              <span className={`heading${idx}`} {...animatedElement[idx]} key={item}>
+                {item}
+              </span>
+            </div>
+          ))}
+        </h1>
+      </div>
     </section>
   );
 };
